6ffc8291d5ab8cba521ea1f0b46fa6eb343a7d50
Peter Prib <peter.prib@gmail.com>
2019-04-18 15:36:24 +1000

    fix bug in random add selection on fold and next with smoothing improve documentation add in management messages

--- README.md
+++ README.md
@@ -1,18 +1,48 @@
-# node-red-contrib-loadbalance
+# node-red-contrib-loadbalance ![loadbalance](loadbalance/icons/icons8-multicast-80.png "Load Balance") 
 
 
 [Node-Red][1] node to [load balance][2].
 
 Basically spreads input messages to flows based on:
-*   round robin - next in list
-*   random - randomly across out paths
+* Round Robin - next in list then start at being again
+* Random - randomly across out paths
+* Fold on capacity - place load on first node in order with capacity.  Good for improving cache hit ratios 
+* Next smoothing to average capacity - next that is >= average capacity to get smoothing of load
+
 
 This allows incoming messages to be passed to servers that may be other node red instances.
 
+![Load Balance](documentation/loadbalance.JPG "Load Balance")
+
+# Management
+
+Messages can be sent to node with the following topics and not forwarded 
+
+## msg.topic loanbalance
+
+Takes in metrics for a path in msg.payload in form:
+
+	{path: <path number>, capacity: <numeric value>} 
+
+or and array of above.
+
+Capacity of zero is considered saturation.  Positive values are expected.
+
+Basically a remote node could be constructed to send a message to update 
+
+## msg.topic loanbalancedebug
+
+Will send metadata about queues to error log so visible in debug console.
+
 
 ## To be done
-* resource based even load selection
-* resource based fold selection
+
+1. Stop and start a path
+* Choice between message discard, queue, random or next when saturation reached 
+* Am alive polling, keep alive can be used to trigger remote to send capacity metrics
+* Dynamic addition of paths by remote nodes subscribing or discovery
+* Some base capacity calls to remote engines 
+
 
 # Install
 
@@ -23,6 +53,7 @@ Run the following command in the root directory of your Node-RED install
 
 Test/example flow in  test/testflow.json
 
+![Test](documentation/test.JPG "Test flow")
 
 # Author
   
--- loadbalance/loadbalance.js
+++ loadbalance/loadbalance.js
@@ -1,32 +1,92 @@
-function random(m) {
+
+var setPaths={
+	random:	function() {
+			this.path=Math.floor(Math.random()*this.outputs);
+	},
+	next: function() {
+		if(--this.path<0) {
+			this.path=this.outputs-1;
+		}   
+	},
+	foldweighted: function() {
+		for(this.path=0;this.path<this.outputs;this.path++) {
+			if(this.paths[this.path].capacity>0) return;
+		}
+		this.random();
+	},
+	nextsmoothed:function() {
+		for(var i=0;i<this.outputs;i++) {
+        	if(--this.path<0) {
+        		this.path=this.outputs-1;
+       		}
+			if(this.paths[this.path].capacity>=this.pathCapacityAvg) return;
+		}
+	},
+	dropmessage:function() {
+		this.path=this.outputs;  // by placing on none existence wire
+	}
 }
- 
+
+
 module.exports = function(RED) {
     function loadBalanceNode(n) {
         RED.nodes.createNode(this,n);
-        var node=Object.assign(this,{path:1},n);
-        switch (node.selection) {
-        	case 'random':
-        		var base=node.outputs+1;
-        		var setPath = function() {
-        			node.path=Math.floor(Math.random()*base);
-        		};
-        		break;
-        	case 'next':
-        		var setPath = function() {
-            		if(!(--node.path)) {
-            			node.path=node.outputs;
-            		}
-        		};
-        		break;
-        	default:
-        		node.error("invalid selection "+node.selection);
+        var node=Object.assign(this,{path:0},n);
+        node.paths=[];
+        for(var i=0;i<node.outputs;i++) {
+        	node.paths.push({status:0,capacity:100,count:0,history:Array(3).fill({count:0,capacity:0})})
         }
-
+        node.pathCapacityAvg=100;
+		var setPath = function() {
+			node.path=Math.floor(Math.random()*node.outputs);
+		};
+		try{
+			var setPath=setPaths[node.selection];
+		} catch(e) {
+			node.error("Selection mode not found: "+node.selection);
+		}
         node.on('input', function (msg) {
-        	setPath();
-        	var o=Array(node.outputs).fill(null).fill(msg,node.path-1,node.path);
+            switch (msg.topic) {
+        		case 'loadbalance':
+        			try {
+        				if(Array.isArray(msg.payload)) {
+        					for(var a in msg.payload) {
+        						node.paths[a.path].capacity=a.capacity;
+        					}
+        				} else {
+        					node.paths[msg.payload.path].capacity=msg.payload.capacity;
+        				}
+        			} catch(e) {
+        				node.error("loadbalance update failed");
+        			}
+        			for(var t=0,i=0;i<node.outputs;i++) {
+        				t+=node.paths[i].capacity
+        			}
+        			node.pathCapacityAvg=t/node.outputs;
+        			node.send();
+        			return;
+        		case 'loadbalancedebug':
+    				node.error("selection mode: "+node.selection+"path pointer: "+node.path+" average capacity: "+node.pathCapacityAvg +" "+JSON.stringify(node.paths));
+    				node.send();
+        			return;
+            }
+        	setPath.apply(this);
+        	node.paths[node.path].count++;
+        	var o=Array(node.outputs).fill(null).fill(msg,node.path,node.path+1);
 			node.send(o);
+        });
+        function checkLoop() { 
+        	for(var n,i=0;i<node.outputs;i++) {
+        		n=node.paths[node.path];
+        		n.history.unshift({status:n.status,count:n.count,capacity:n.capacity});
+        		n.history.pop();
+        		n.count=0;
+        	}
+        }
+        node.check = setInterval(checkLoop, 60000); // check every minute 
+        node.on("close", function(removed,done) {
+        	clearInterval(node.check);
+        	done();
         });
     }
     RED.nodes.registerType("Load Balance",loadBalanceNode);
